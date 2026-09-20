94cfe67067e28a29f39f6a75ca9fea7c32a59d8b
sonikathakur279-ai <sonikathakur279@gmail.com>
2026-09-20 17:19:57 +0530

    Create Color.kt

---
+++ app/src/main/java/com/hybridtts/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.hybridtts.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PureBlack = Color(0xFF000000)
+val DarkSurface = Color(0xFF0A0D12)
+val DarkCard = Color(0xFF141820)
+val AccentEmerald = Color(0xFF00E676)
+val AccentCyan = Color(0xFF00B0FF)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFF9098A9)
+val BorderSubtle = Color(0xFF222834)
+val StatusWarning = Color(0xFFFFB300)
+val StatusError = Color(0xFFFF5252)
