a57b6d3cad4b47c636d054c987b7820080e6fe5e
sonikathakur279-ai <sonikathakur279@gmail.com>
2026-09-23 21:06:57 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hybridtts"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.0-DAY6"
+        versionCode = 7
+        versionName = "1.0.0-DAY7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
